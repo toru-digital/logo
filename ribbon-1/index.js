c7fecfcc41f9fdbf1a28475b0b089a59de6c6393
Sean <>
2023-08-05 16:28:58 +0100

    strip back

--- ribbon-1/index.js
+++ ribbon-1/index.js
@@ -11,29 +11,12 @@ let pointerXOnPointerDown = 0;
 let windowHalfX = window.innerWidth / 2;
 let logoTexture
 let shapes = [];
-let materials = [
-	new THREE.MeshBasicMaterial({
-		 color: 0x000000
-	}),
-	new THREE.MeshBasicMaterial({
-		 color: 0x000000
-	})
-]
 
 let settings = {
 	letter_size : 150,
 	tracking : 200,
 	corners : 0.75,
-	bevelSize: 0,
-};
-
-const extrudeSettings = {
-	steps: 1,
-	bevelEnabled: true,
-	bevelThickness: 1,
-	bevelOffset: 0,
-	bevelSegments: 1,
-	depth : 1000
+	depth : 100,
 };
 
 buildScene ();
@@ -123,105 +106,6 @@ function getT () {
 	return mesh
 }
 
-function getO () {
-	let radius = settings.letter_size * 0.5;
-	let segments = 64;
-	let theta, x, y
-	let theta_next, x_next, y_next, j;
-	let shape = new THREE.Shape()
-
-	for (let i = 0; i < segments; i++) {
-		theta = ((i + 1) / segments) * Math.PI * 2.0;
-		x = radius * Math.cos(theta);
-		y = radius * Math.sin(theta);
-		j = i + 2;
-		if( (j - 1) === segments ) j = 1;
-		theta_next = (j / segments) * Math.PI * 2.0;
-		x_next = radius * Math.cos(theta_next);
-		y_next = radius * Math.sin(theta_next);
-
-		// console.log (x, y, x_next, y_next)
-
-		shape.moveTo(0, 0);
-		shape.lineTo(x, y);
-		shape.lineTo(x_next, y_next);
-	}
-
-	let geometry = new THREE.ExtrudeGeometry (shape, {...extrudeSettings, bevelSize: settings.bevelSize});
-	let mesh = new THREE.Mesh ( 
-		geometry, 
-		materials
-	);
-	return mesh
-}
-
-function getR () {
-	const offset = settings.letter_size * -0.5;
-
-	const size = settings.letter_size;
-	const shape = new THREE.Shape()
-		.moveTo (
-			0 + offset, 
-			0 + offset
-		)
-		.lineTo (
-			size + offset, 
-			0 + offset
-		)
-		.lineTo (
-			size + offset, 
-			size + offset
-		)
-		.lineTo (
-			0 + offset, 
-			size + offset
-		)
-		.lineTo (
-			0 + offset, 
-			0 + offset
-		)
-
-	let geometry = new THREE.ExtrudeGeometry (shape, {...extrudeSettings, bevelSize: settings.bevelSize});
-	let mesh = new THREE.Mesh ( 
-		geometry, 
-		materials
-	);
-	return mesh
-}
-
-function getU () {
-
-	const corner_multiplier = settings.corners * 0.3
-
-	const corner = Math.min (settings.letter_size * corner_multiplier, settings.letter_size * 0.25)
-	const offset = settings.letter_size * -0.5;
-
-	const shape = new THREE.Shape()
-		.moveTo (settings.letter_size - corner * 2 + offset, 0 + offset)
-		.lineTo (corner * 2 + offset, 0 + offset)
-		.bezierCurveTo (
-			0 + offset, 0 + offset, 
-			0 + offset, corner * 2 + offset, 
-			0 + offset, corner * 2 + offset
-		)
-		.lineTo (0 + offset, settings.letter_size + offset)
-		.lineTo (settings.letter_size + offset, settings.letter_size + offset)
-		.lineTo (settings.letter_size + offset, corner * 2 + offset)
-		.bezierCurveTo (
-			settings.letter_size + offset, corner*2 + offset, 
-			settings.letter_size + offset, 0 + offset, 
-			settings.letter_size - corner * 2 + offset, 0 + offset
-		)
-
-	let geometry = new THREE.ExtrudeGeometry (shape, {...extrudeSettings, bevelSize: settings.bevelSize});
-	let mesh = new THREE.Mesh ( 
-		geometry, 
-		materials
-	);
-
-	return mesh
-}
-
 function buildLogo () {
 
 	shapes.forEach (shape => {
@@ -242,7 +126,8 @@ function buildControls () {
 	gui.add (settings, 'letter_size', 0.1, 400)
 	gui.add (settings, 'tracking', 0, 600)
 	gui.add (settings, 'corners', 0, 1)
-	gui.add (settings, 'bevelSize', 0, 6)
+	gui.add (settings, 'depth', 0, 500)
+	
 }
 
 function onWindowResize () {
