6591613dbfd1320608f02e3bd85d64ba944118a7
Sean <>
2023-08-05 13:15:44 +0100

    get preview right

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				arabic1: resolve(__dirname, 'arabic-1/index.html'),
+				flat1: resolve(__dirname, 'flat-1/index.html'),
+			},
+		},
+	},
+})
