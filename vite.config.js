60ee23490eda0d511fc7a441e66fa5b0b89c95c9
Sean <>
2023-08-06 14:01:14 +0100

    add ribbon 2

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
 				flat2: resolve(__dirname, 'flat-2/index.html'),
 				flat3: resolve(__dirname, 'flat-3/index.html'),
 				ribbon1: resolve(__dirname, 'ribbon-1/index.html'),
+				ribbon2: resolve(__dirname, 'ribbon-2/index.html'),
 			},
 		},
 	},
