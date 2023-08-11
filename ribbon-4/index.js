8993973e71104e8cd9d275a471fb6357d5540183
Sean <>
2023-08-11 14:42:37 +0100

    getT

--- ribbon-4/index.js
+++ ribbon-4/index.js
@@ -1,10 +1,9 @@
 // https://discourse.threejs.org/t/how-to-do-text-mask-and-used-three-js-component-as-a-background/49594/3
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import * as THREE from "three";
+import { TWEEN } from '../tween.module.min';
+// console.clear();
 
-console.clear();
-
-let gu = {
+let settings = {
 	time: { value: 0 },
 	mask: { value: getMask()},
 	color_1 : '0.0',
@@ -12,16 +11,15 @@ let gu = {
 	color_3: '0.0',
 	color_4: '1.8',
 	color_5: '0.5',
-	color_6: '1.0',
+	color_6: '1.0'
 };
 
 let scene = new THREE.Mesh(
 	new THREE.PlaneGeometry(2, 2),
 	new THREE.MeshBasicMaterial({
-		//map: gu.mask.value,
 		onBeforeCompile: shader => {
-			shader.uniforms.time = gu.time;
-			shader.uniforms.mask = gu.mask;
+			shader.uniforms.time = settings.time;
+			shader.uniforms.mask = settings.mask;
 			shader.fragmentShader = `
 			uniform float time;
 			uniform sampler2D mask;
@@ -37,9 +35,9 @@ let scene = new THREE.Mesh(
 			vec3 neonGradient(float t) {
 				return clamp(
 					vec3(
-						t * ` + gu.color_1 + ` + ` + gu.color_2 + `, 
-						square(abs(` + gu.color_3 + ` - t) * ` + gu.color_4 + `), 
-						(` + gu.color_5 + ` - t) * ` + gu.color_6 + `
+						square(abs(` + settings.color_3 + ` - t) * ` + settings.color_4 + `),
+						square(abs(` + settings.color_3 + ` - t) * ` + settings.color_4 + `),
+						t * ` + settings.color_1 + ` + ` + settings.color_2 + `
 					), 
 					0.0, 
 					1.0
@@ -73,7 +71,7 @@ let camera = new THREE.Camera();
 let renderer = new THREE.WebGLRenderer({
 	antialias: true
 });
-renderer.setSize(1024, 512);
+renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor(0x404040);
 document.body.appendChild(renderer.domElement);
 
@@ -81,16 +79,60 @@ let clock = new THREE.Clock();
 
 renderer.setAnimationLoop((_) => {
 	let t = clock.getElapsedTime();
-	gu.time.value = t * 0.05;
+	settings.time.value = t * 0.05;
 	renderer.render(scene, camera);
 });
 
+function getT () {
+	const letter_size = 320;
+	const corners = 0.75;
+	const depth = 1000;
+
+	const corner_multiplier = 0.4 + 0.6 * (1-corners);
+
+	const middle = Math.min (letter_size * corner_multiplier, letter_size);
+	const corner = Math.max ((letter_size - middle) * 0.5, 0);
+	const offset = letter_size * -0.5;
+
+	const vertices_2d = [
+		[corner, 0],
+		[corner + middle,  0],
+		[corner + middle, corner],
+		[letter_size, corner],
+		[letter_size, corner + middle],
+		[corner + middle, corner + middle],
+		[corner + middle, letter_size],
+		[corner, letter_size],
+		[corner, corner + middle],
+		[0, corner + middle],
+		[0, corner],
+		[corner, corner]
+	];
+
+	let vertices_3d = [];
+
+	vertices_2d.forEach ((v, index) => {
+		const v1 = v;
+		const v2 = vertices_2d [(index + 1) % vertices_2d.length];
+
+		vertices_3d = vertices_3d.concat ([
+			v1[0] + offset, v1[1] + offset, 0,
+			v2[0] + offset, v2[1] + offset, 0,
+			v2[0] + offset, v2[1] + offset, -1*depth,
+
+			v2[0] + offset, v2[1] + offset, -1*depth,
+			v1[0] + offset, v1[1] + offset, -1*depth,
+			v1[0] + offset, v1[1] + offset, 0
+		])
+	})
+}
+
 function getMask(){
 	let c = document.createElement("canvas");
-	c.width = 1024;
-	c.height = 512;
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
 	let ctx = c.getContext("2d");
-	for(let i = 0; i < 50; i++){
+	for(let i = 0; i < 1; i++){
 		let val = Math.floor(Math.random() * 255);
 		let color = `rgb(${val}, ${val}, ${val})`;
 		let size = Math.floor(Math.random() * 5) * 50;
@@ -101,16 +143,13 @@ function getMask(){
 		ctx.fillRect(-size * 0.5, -size * 0.5, size, size);
 		ctx.restore();
 	}
+
+	// getT ();
 	let tex = new THREE.CanvasTexture(c);
 	return tex;
 }
 
 function buildControls () {
-	const gui = new GUI()
-
-	const colorsFolder = gui.addFolder ('Colours')
-
-	colorsFolder.add (gu, 'color_1', 0, 5)
 }
 
 buildControls ()
