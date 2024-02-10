a0dc69d5d7116a9baddda9c925d1e52b5cc6b255
Sean <>
2024-02-10 10:11:52 +0000

    don't show

--- grid-1/script.js
+++ grid-1/script.js
@@ -38,78 +38,56 @@ const shapes = []
 const draw = SVG().addTo('#logo-container')
 
 const drawLogo = () => {
-	const gap = settings.size + settings.padding;
-
-	draw.clear()
-
-	const drawT = i => {
+	const drawT = () => {
 		const s1 = settings.size;
 		const s2 = settings.size * 0.2 * settings.growth;
 		const s3 = s1 - s2;
 
-		shapes[i] = draw
+		return draw
 			.path(`M ${s2}  0 L ${s3} 0 L ${s3} ${s2} L ${s1} ${s2} L ${s1} ${s3} L ${s3} ${s3} L ${s3} ${s1} L ${s2} ${s1} L ${s2} ${s3} L 0 ${s3} L 0 ${s2} L ${s2} ${s2} L ${s2} 0`)
-			.move(
-				settings.padding + i * gap,
-				settings.size * -0.5 + settings.padding + settings.size * 0.5
-			)
-			.attr({
-				fill: colours[scheme].logo
-			})
+			.attr({fill: colours[scheme].logo})
+			.hide ()
 	}
 
-	const drawO = i => {
-		shapes[i] = draw
+	const drawO = () => {
+		return draw
 			.rect(settings.size, settings.size)
 			.radius(settings.size * 0.5 * settings.growth, settings.size * 0.5 * settings.growth)
-			.move(
-				settings.padding + i * gap,
-				settings.size * -0.5 + settings.padding + settings.size * 0.5
-			)
-			.attr({
-				fill: colours[scheme].logo
-			})
+			.attr({fill: colours[scheme].logo})
+			.hide ()
 	}
 
-	const drawR = i => {
-		shapes[i] = draw.rect(settings.size, settings.size)
-			.move(
-				settings.padding + i * gap,
-				settings.size * -0.5 + settings.padding + settings.size * 0.5
-			)
-			.attr({
-				fill: colours[scheme].logo
-			})
+	const drawR = () => {
+		return draw.rect(settings.size, settings.size)
+			.attr({fill: colours[scheme].logo})
+			.hide ()
 	}
 
-	const drawU = i => {
+	const drawU = () => {
 		const s1 = settings.size
 		const s2 = s1 / 3
 		const s3 = s1 - s2
 		const s4 = s1 * 0.15 * settings.growth
 		const s5 = s1 - s4
+  
+		return draw
+		  .path(`
+			 M 0 0 
+			 H ${s1} 
+			 V ${s3} 
+			 C ${s1} ${s5} ${s5} ${s1} ${s3} ${s1} 
+			 H ${s2} 
+			 C ${s4} ${s1} 0 ${s5} 0 ${s3}`)
+		  .attr({
+			 fill: colours[scheme].logo
+		  })
+		  .hide ()
+	 }
 
-		shapes[i] = draw
-			.path(`
-				M 0 0 
-				H ${s1} 
-				V ${s3} 
-				C ${s1} ${s5} ${s5} ${s1} ${s3} ${s1} 
-				H ${s2} 
-				C ${s4} ${s1} 0 ${s5} 0 ${s3}`)
-			.move(
-				settings.padding + i * gap,
-				settings.size * -0.5 + settings.padding + settings.size * 0.5
-			)
-			.attr({
-				fill: colours[scheme].logo
-			})
-	}
-
-	drawT(0)
-	drawO(1)
-	drawR(2)
-	drawU(3)
+	shapes[0] = drawT ()
+	shapes[1] = drawO ()
+	shapes[2] = drawR ()
+	shapes[3] = drawU ()
 }
 
 const drawGrid = () => {
@@ -124,14 +102,12 @@ const drawGrid = () => {
 	const num_rows = Math.ceil (container_height / (settings.size + settings.padding))
 	
 	// for (let x = 0; x < num_cols; x++) {
-	// 		for (let y = 0; y < num_rows; y++) {
-		
+	// 	for (let y = 0; y < num_rows; y++) {
 	// 		if (x < 4  && y === 0) continue;
-			
-	// 		draw.use (shapes[Math.floor(Math.random() * 4)]).move (
-	// 		start + x * spacer, 
-	// 		start + y * spacer
-	// 	)
+	// 			draw.use (shapes[Math.floor(Math.random() * 4)]).move (
+	// 			start + x * spacer, 
+	// 			start + y * spacer
+	// 		)
 	// 	}
 	// }
 }
@@ -142,6 +118,7 @@ const setColours = s => {
 	document.getElementById("controls-container").style.backgroundColor = colours[scheme].controls
 	Array.from(document.getElementsByClassName("text")).forEach(e => e.style.color = colours[scheme].text)
 
+	draw.clear()
 	drawLogo()
 	drawGrid()
 }
