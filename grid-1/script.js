6310d716166fe899f345e99ecee17ba0d8e5a0bf
Sean <>
2024-02-10 09:59:25 +0000

    knock out controls. tabs

--- grid-1/script.js
+++ grid-1/script.js
@@ -43,67 +43,67 @@ const drawLogo = () => {
 	draw.clear()
 
 	const drawT = i => {
-	const s1 = settings.size;
-	const s2 = settings.size * 0.2 * settings.growth;
-	const s3 = s1 - s2;
+		const s1 = settings.size;
+		const s2 = settings.size * 0.2 * settings.growth;
+		const s3 = s1 - s2;
 
-	shapes[i] = draw
-		.path(`M ${s2}  0 L ${s3} 0 L ${s3} ${s2} L ${s1} ${s2} L ${s1} ${s3} L ${s3} ${s3} L ${s3} ${s1} L ${s2} ${s1} L ${s2} ${s3} L 0 ${s3} L 0 ${s2} L ${s2} ${s2} L ${s2} 0`)
-		.move(
-			settings.padding + i * gap,
-			settings.size * -0.5 + settings.padding + settings.size * 0.5
-		)
-		.attr({
-			fill: colours[scheme].logo
-		})
+		shapes[i] = draw
+			.path(`M ${s2}  0 L ${s3} 0 L ${s3} ${s2} L ${s1} ${s2} L ${s1} ${s3} L ${s3} ${s3} L ${s3} ${s1} L ${s2} ${s1} L ${s2} ${s3} L 0 ${s3} L 0 ${s2} L ${s2} ${s2} L ${s2} 0`)
+			.move(
+				settings.padding + i * gap,
+				settings.size * -0.5 + settings.padding + settings.size * 0.5
+			)
+			.attr({
+				fill: colours[scheme].logo
+			})
 	}
 
 	const drawO = i => {
-	shapes[i] = draw
-		.rect(settings.size, settings.size)
-		.radius(settings.size * 0.5 * settings.growth, settings.size * 0.5 * settings.growth)
-		.move(
-			settings.padding + i * gap,
-			settings.size * -0.5 + settings.padding + settings.size * 0.5
-		)
-		.attr({
-			fill: colours[scheme].logo
-		})
+		shapes[i] = draw
+			.rect(settings.size, settings.size)
+			.radius(settings.size * 0.5 * settings.growth, settings.size * 0.5 * settings.growth)
+			.move(
+				settings.padding + i * gap,
+				settings.size * -0.5 + settings.padding + settings.size * 0.5
+			)
+			.attr({
+				fill: colours[scheme].logo
+			})
 	}
 
 	const drawR = i => {
-	shapes[i] = draw.rect(settings.size, settings.size)
-		.move(
-			settings.padding + i * gap,
-			settings.size * -0.5 + settings.padding + settings.size * 0.5
-		)
-		.attr({
-			fill: colours[scheme].logo
-		})
+		shapes[i] = draw.rect(settings.size, settings.size)
+			.move(
+				settings.padding + i * gap,
+				settings.size * -0.5 + settings.padding + settings.size * 0.5
+			)
+			.attr({
+				fill: colours[scheme].logo
+			})
 	}
 
 	const drawU = i => {
-	const s1 = settings.size
-	const s2 = s1 / 3
-	const s3 = s1 - s2
-	const s4 = s1 * 0.15 * settings.growth
-	const s5 = s1 - s4
+		const s1 = settings.size
+		const s2 = s1 / 3
+		const s3 = s1 - s2
+		const s4 = s1 * 0.15 * settings.growth
+		const s5 = s1 - s4
 
-	shapes[i] = draw
-		.path(`
-			M 0 0 
-			H ${s1} 
-			V ${s3} 
-			C ${s1} ${s5} ${s5} ${s1} ${s3} ${s1} 
-			H ${s2} 
-			C ${s4} ${s1} 0 ${s5} 0 ${s3}`)
-		.move(
-			settings.padding + i * gap,
-			settings.size * -0.5 + settings.padding + settings.size * 0.5
-		)
-		.attr({
-			fill: colours[scheme].logo
-		})
+		shapes[i] = draw
+			.path(`
+				M 0 0 
+				H ${s1} 
+				V ${s3} 
+				C ${s1} ${s5} ${s5} ${s1} ${s3} ${s1} 
+				H ${s2} 
+				C ${s4} ${s1} 0 ${s5} 0 ${s3}`)
+			.move(
+				settings.padding + i * gap,
+				settings.size * -0.5 + settings.padding + settings.size * 0.5
+			)
+			.attr({
+				fill: colours[scheme].logo
+			})
 	}
 
 	drawT(0)
