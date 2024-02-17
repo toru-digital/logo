efb810fe1a4a84787fef2e001e0ca4536aca5df7
Sean <>
2024-02-17 09:43:35 +0000

    tidy

---
+++ grid-1/_settings.js
@@ -0,0 +1,34 @@
+export const colours = {
+	"white-on-black": {
+		background: "#000",
+		controls: "rgb(17,17,17)",
+		text: "#FFF",
+		logo: "#FFF"
+	},
+	"black-on-white": {
+		background: "#FFF",
+		controls: "#FAFAFA",
+		text: "#000",
+		logo: "#000"
+	},
+	"black-on-yellow": {
+		background: "#EDFE06",
+		controls: "#EDFE06",
+		text: "#000",
+		logo: "#000"
+	},
+	"dark": {
+		background: "#000",
+		controls: "#000",
+		text: "#000",
+		logo_background: "#191919",
+		logo: "#FFF"
+	},
+}
+
+export const settings = {
+	size: 40,
+	padding: 6,
+	growth: 1,
+	scheme: "dark"
+}
--- grid-1/script.js
+++ grid-1/script.js
@@ -1,80 +1,7 @@
-
-const colours = {
-	"white-on-black": {
-		background: "#000",
-		controls: "rgb(17,17,17)",
-		text: "#FFF",
-		logo: "#FFF"
-	},
-	"black-on-white": {
-		background: "#FFF",
-		controls: "#FAFAFA",
-		text: "#000",
-		logo: "#000"
-	},
-	"black-on-yellow": {
-		background: "#EDFE06",
-		controls: "#EDFE06",
-		text: "#000",
-		logo: "#000"
-	},
-	"dark": {
-		background: "#000",
-		controls: "#000",
-		text: "#000",
-		logo_background: "#191919",
-		logo: "#FFF"
-	},
-}
-
-const settings = {
-	size: 40,
-	padding: 6,
-	growth: 1
-}
-
-let scheme = "dark"
+import {colours, settings} from './_settings.js';
+import {drawT, drawO, drawR, drawU} from './_shapes.js';
 
 const draw = SVG().addTo('#logo-container')
-
-const drawT = () => {
-	const s1 = settings.size;
-	const s2 = settings.size * 0.2 * settings.growth;
-	const s3 = s1 - s2;
-
-	draw.defs().group ().addClass('letter-0')
-		.path(`M ${s2}  0 L ${s3} 0 L ${s3} ${s2} L ${s1} ${s2} L ${s1} ${s3} L ${s3} ${s3} L ${s3} ${s1} L ${s2} ${s1} L ${s2} ${s3} L 0 ${s3} L 0 ${s2} L ${s2} ${s2} L ${s2} 0`)
-		
-}
-
-const drawO = () => {
-	return draw.defs().group ().addClass('letter-1')
-		.rect(settings.size, settings.size)
-		.radius(settings.size * 0.5 * settings.growth, settings.size * 0.5 * settings.growth)
-}
-
-const drawR = () => {
-	draw.defs().group ().addClass('letter-2')
-		.rect(settings.size, settings.size)
-}
-
-const drawU = () => {
-	const s1 = settings.size
-	const s2 = s1 / 3
-	const s3 = s1 - s2
-	const s4 = s1 * 0.15 * settings.growth
-	const s5 = s1 - s4
-
-	draw.defs ().group ().addClass('letter-3')
-		.path(`
-		M 0 0 
-		H ${s1} 
-		V ${s3} 
-		C ${s1} ${s5} ${s5} ${s1} ${s3} ${s1} 
-		H ${s2} 
-		C ${s4} ${s1} 0 ${s5} 0 ${s3}`
-	)
-}
 
 const drawGrid = () => {
 
@@ -96,7 +23,7 @@ const drawGrid = () => {
 			is_main_logo = y == 0 && x < 4
 			shape_index = is_main_logo ? x : Math.floor(Math.random()  * 4);
 			shape_class = ".letter-" + shape_index
-			shape_colour = is_main_logo ? colours[scheme].logo : colours[scheme].logo_background
+			shape_colour = is_main_logo ? colours[settings.scheme].logo : colours[settings.scheme].logo_background
 
 			draw.use (draw.defs().findOne(shape_class))
 			.attr({fill: shape_colour})
@@ -108,25 +35,17 @@ const drawGrid = () => {
 }
 
 const setColours = s => {
-	scheme = s
-	document.body.style.backgroundColor = colours[scheme].background
-	document.getElementById("controls-container").style.backgroundColor = colours[scheme].controls
-	Array.from(document.getElementsByClassName("text")).forEach(e => e.style.color = colours[scheme].text)
+	settings.scheme = s
+	document.body.style.backgroundColor = colours[settings.scheme].background
+	document.getElementById("controls-container").style.backgroundColor = colours[settings.scheme].controls
+	Array.from(document.getElementsByClassName("text")).forEach(e => e.style.color = colours[settings.scheme].text)
 
 	draw.clear()
-	drawT ()
-	drawO ()
-	drawR ()
-	drawU ()
+	drawT (draw)
+	drawO (draw)
+	drawR (draw)
+	drawU (draw)
 	drawGrid()
 }
 
-setColours(scheme)
-
-const start = () => { 
-	settings.size = Math.round(document.getElementById("size").value);
-	settings.padding = Math.round(document.getElementById("padding").value);
-
-	scheme = document.getElementById("scheme").value;
-	setColours(scheme)
-}
+setColours(settings.scheme)
