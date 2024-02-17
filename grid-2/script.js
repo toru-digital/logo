b954fca00c8fb4807eb4739d6dc8a7cf1c2729b3
Sean <>
2024-02-17 10:24:39 +0000

    block function

--- grid-2/script.js
+++ grid-2/script.js
@@ -6,33 +6,37 @@ const draw = SVG().addTo('#logo-container')
 let interval
 const blocks = []
 
-const drawGrid = () => {
-	const {num_rows, num_cols} = getRowsAndCols ()
+const drawBlock = (x, y) => {
 	const spacer = settings.padding + settings.size;
 	const start = settings.padding;
 	let shape_class, shape_index, is_main_logo, block
 
+	is_main_logo = y == 0 && x < 4
+	shape_index = is_main_logo ? x : Math.floor(Math.random()  * 4);
+	shape_class = ".letter-" + shape_index
+
+	block = draw.use (draw.defs().findOne(shape_class))
+		.attr({
+			fill: colours[settings.scheme].logo,
+			opacity : 0
+		})
+		.move (
+			start + x * spacer, 
+			start + y * spacer )
+
+	return{
+		x: x,
+		y: y,
+		block: block,
+		is_main_logo: is_main_logo
+	}
+}
+
+const drawGrid = () => {
+	const {num_rows, num_cols} = getRowsAndCols ()
 	for (let x = 0; x < num_cols; x++) {
 		for (let y = 0; y < num_rows; y++) {
-			is_main_logo = y == 0 && x < 4
-			shape_index = is_main_logo ? x : Math.floor(Math.random()  * 4);
-			shape_class = ".letter-" + shape_index
-
-			block = draw.use (draw.defs().findOne(shape_class))
-				.attr({
-					fill: colours[settings.scheme].logo,
-					opacity : 0
-				})
-				.move (
-					start + x * spacer, 
-					start + y * spacer )
-
-			blocks.push ({
-				x: x,
-				y: y,
-				block: block,
-				is_main_logo: is_main_logo
-			})
+			blocks.push (drawBlock (x,y))
 		}
 	}
 }
