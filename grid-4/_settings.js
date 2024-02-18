a9e92896958a52d5850dfb4451de63025a327b7e
Sean <>
2024-02-18 09:47:52 +0000

    grid 4

---
+++ grid-4/_settings.js
@@ -0,0 +1,14 @@
+export const colours = {
+	"dark": {
+		background: "#000",
+		logo_background: "rgba(255, 255, 255, 0.1)",
+		logo: "rgba(255, 255, 255, 1)"
+	},
+}
+
+export const settings = {
+	size: 40,
+	padding: 6,
+	growth: 1,
+	scheme: "dark"
+}
