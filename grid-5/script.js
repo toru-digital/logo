d235aecafcc635e14a8b962d9c5d0240a9313904
Sean <>
2024-02-22 13:06:52 +0000

    movement

--- grid-5/script.js
+++ grid-5/script.js
@@ -22,51 +22,48 @@ const food = {
 }
 
 const turnUp = () => {
-	restart ()
-	// console.log ("U")
-	// if ( dir == 'down' ) return
-	// dir = 'up';
-	// snake.dirY = -settings.sizeCell;
-	// snake.dirX = 0;
+	if ( dir == 'down' ) return
+	dir = 'up';
+	snake.dirY = -1;
+	snake.dirX = 0;
 }
 
 const turnLeft = () => {
-	console.log ("L")
 	if ( dir == 'right' ) return
 	dir = 'left';
-	snake.dirX = -settings.sizeCell;
+	snake.dirX = -1;
 	snake.dirY = 0;
 }
 
 const turnDown = () => {
-	console.log ("D")
 	if ( dir == 'up' ) return
 	dir = 'down';
-	snake.dirY = settings.sizeCell;
+	snake.dirY = 1;
 	snake.dirX = 0;
 }
 
 const turnRight = () => {
-	console.log ("R")
 	if ( dir == 'left' ) return
 	dir = 'right';
-	snake.dirX = settings.sizeCell;
+	snake.dirX = 1;
 	snake.dirY = 0;
 }
 
-// const checkBorder = () => { 
-// 	if ( snake.x < 0 ) {
-// 		snake.x = canvas.width - settings.sizeCell;
-// 	} else if ( snake.x >= canvas.width ) {
-// 		snake.x = 0;
-// 	}
+const checkBorder = () => { 
+	const {num_rows, num_cols} = getRowsAndCols ()
 
-// 	if ( snake.y < 0 ) {
-// 		snake.y = canvas.height - settings.sizeCell;
-// 	} else if ( snake.y >= canvas.height ) {
-// 		snake.y = 0;
-// 	}
-// }
+	if ( snake.x < 0 ) {
+		snake.x = num_cols;
+	} else if ( snake.x >= num_cols ) {
+		snake.x = 0;
+	}
+
+	if ( snake.y < 0 ) {
+		snake.y = num_rows;
+	} else if ( snake.y >= num_rows ) {
+		snake.y = 0;
+	}
+}
 
 const randomPosFood = () => {
 	const {num_rows, num_cols} = getRowsAndCols ()
@@ -75,19 +72,19 @@ const randomPosFood = () => {
 }
 
 const restart = () => {
-	// const {num_rows, num_cols} = getRowsAndCols ()
+	const {num_rows, num_cols} = getRowsAndCols ()
 
-	// settings.stepMax = 6;
-	// scoreCount = 0;
+	settings.stepMax = 6;
+	scoreCount = 0;
 
-	// snake.x = randomInt(0, num_cols);
-	// snake.y = randomInt(0, num_rows);
-	// snake.body = [];
-	// snake.maxBodySize = 1;
-	// snake.dirX = 0;
-	// snake.dirY = 0;
+	snake.x = randomInt(0, num_cols);
+	snake.y = randomInt(0, num_rows);
+	snake.body = [];
+	snake.maxBodySize = 1;
+	snake.dirX = 0;
+	snake.dirY = 0;
 
-	// dir = '';
+	dir = '';
 
 	randomPosFood();
 }
@@ -113,7 +110,7 @@ const drawBlock = (x, y) => {
 		x: x,
 		y: y,
 		block: block,
-		chaos: 0
+		is_snake : false
 	}
 }
 
@@ -144,16 +141,16 @@ const update = () => {
 	blocks.forEach ((block) => {
 		let opacity, colour
 		const is_food = block.x == food.x && block.y == food.y
+		const is_snake = snake.body.some (e => e.x == block.x && e.y == block.y)
 
-		// change_shape = Math.random() < 0.3 * block.chaos
-		opacity = is_food ? 1 : 0.1; //Math.random()*0.3 * block.chaos
+		opacity = is_food || is_snake ? 1 : 0.1;
 		colour = is_food ? "#EDFE06" : "white"
 
 		block.block.opacity (opacity)
 		block.block.fill (colour)
-
-		block.chaos = Math.max(0, block.chaos - 0.03)
 	})
+
+	drawSnake();
 }
 
 clearInterval (interval)
@@ -188,33 +185,32 @@ document.addEventListener('keydown', (e) => {
 // 	else if ( scoreCount <= 15 ) config.stepMax = 6;
 // }
 
-// function drawSnake() {
-// 	snake.x += snake.dirX;
-// 	snake.y += snake.dirY;
+const drawSnake = () => {
+	snake.x += snake.dirX;
+	snake.y += snake.dirY;
 
-// 	checkBorder();
+	checkBorder();
 
-// 	snake.body.unshift({x: snake.x, y: snake.y});
-// 	if ( snake.body.length > snake.maxBodySize ) {
-// 		snake.body.pop();
-// 	}
+	snake.body.unshift ({x: snake.x, y: snake.y});
 
-// 	snake.body.forEach((e, index) => {
+	if (snake.body.length > snake.maxBodySize) {
+		snake.body.pop();
+	}
 
-// 		ctx.fillStyle = '#FFFFFF';
-// 		ctx.fillRect(e.x, e.y, config.sizeCell, config.sizeCell);
+	// snake.body.forEach((e, index) => {
 
-// 		if ( e.x == food.x && e.y == food.y ) {
 
-// 			score();
-// 			randomPosFood();
-// 			snake.maxBodySize++;
-// 		}
+		// if ( e.x == food.x && e.y == food.y ) {
 
-// 		for ( let i = index + 1; i < snake.body.length; i++ ) {
-// 			if ( e.x === snake.body[i].x && e.y === snake.body[i].y ) {
-// 				restart();
-// 			}
-// 		}
-// 	});
-// }
+		// 	score();
+		// 	randomPosFood();
+		// 	snake.maxBodySize++;
+		// }
+
+	// 	for ( let i = index + 1; i < snake.body.length; i++ ) {
+	// 		if ( e.x === snake.body[i].x && e.y === snake.body[i].y ) {
+	// 			restart();
+	// 		}
+	// 	}
+	// });
+}
